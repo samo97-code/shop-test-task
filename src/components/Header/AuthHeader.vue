af235afb4192d168004e6e2352bcc89f5553f213
Samvel <samo97danielyan@gmail.com>
2023-06-14 14:08:02 +0400

    add project

--- src/components/Header/AuthHeader.vue
+++ src/components/Header/AuthHeader.vue
@@ -1,7 +1,7 @@
 <template>
  <div class="sticky top-0 h-[60px] w-full bg-[#676b70] flex items-center z-[1]">
    <div class="main-container flex items-center justify-between">
-     <img src="src/assets/images/logo.png" alt="logo" class="w-[40px] h-[40px]">
+     <img src="../assets/images/logo.png" alt="logo" class="w-[40px] h-[40px]">
 
      <div class="flex gap-x-4">
        <router-link to="/">
